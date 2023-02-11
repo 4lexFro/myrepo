e4415398a5957df8acef366538414af076b6fe3a
Alexey Frolov <k43bfg@gmail.com>
2023-02-11 19:29:19 +0300

    Завершили задачу №4

--- DZ_1.1/№4/Program.cs
+++ DZ_1.1/№4/Program.cs
@@ -4,4 +4,23 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+Console.WriteLine("Введите a ");
+Console.WriteLine("Введите b ");
+Console.WriteLine("Введите c ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max;
+max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+Console.Write("max = ");
+Console.WriteLine(max);
 
