b8c410d4fea95528809ac927b2ed98e27b9a5e0d
Alexey Frolov <k43bfg@gmail.com>
2023-02-14 11:39:44 +0300

    Сделали  задачу нахождения элемента массива из n чисел,равного find

---
+++ lec_2/MethodArray/Program.cs
@@ -0,0 +1,19 @@
+﻿// Имеется массив из n элементов ,
+// требуется найти элемент массива ,равный find
+
+int[] array = { 1, 34, 75, 51, 45, 38, 7, 68, 66, 5, };
+
+int n = array.Length;
+int find = 68;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    // index = index + 1
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
