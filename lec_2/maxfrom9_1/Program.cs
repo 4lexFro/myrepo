afb57177bf2d6616ad8c4b94ddcabc195c6f09e6
Alexey Frolov <k43bfg@gmail.com>
2023-02-14 10:37:41 +0300

    Закончили задачу max из 9 чисел c функцией max и дополнениями

--- lec_2/maxfrom9_1/Program.cs
+++ lec_2/maxfrom9_1/Program.cs
@@ -18,11 +18,13 @@ int b2 = 7;
 int c2 = 435;
 int a3 = 86;
 int b3 = 15;
-int c3 = 3;
+int c3 = 849850939;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max (max1, max2, max3);
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max (max1, max2, max3);
+
+int max = Max ( Max(a1, b1, c1),Max(a2, b2, c2),Max(a3, b3, c3));
 
 Console.WriteLine (max);
