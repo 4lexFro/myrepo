7e33b66174e3a578444685852369316f660ba7e7
Alexey Frolov <k43bfg@gmail.com>
2023-02-15 16:41:16 +0300

    целые числа от -N до N

---
+++ sem_1/001/Program.cs
@@ -0,0 +1,18 @@
+﻿// На входе принимает одно число N,
+// а на выходе показывает все целые числа от -N до N
+
+Console.WriteLine("Start");
+
+Console.Write("Введите N: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+// или
+// int N = int.Parse(Console.Readline());
+int index = -N;
+while (index <= N)
+{
+    Console.WriteLine(index);
+    index = index + 1;
+}
+
+Console.WriteLine("End");
