bfe25ef8ba302752045fc0801e8910fb3d4492bb
Alexey Frolov <k43bfg@gmail.com>
2023-02-16 14:41:17 +0300

    Принимает число и проверяет,кратно ли оно 7 и 23. Второй вариант.

--- sem_2/002/Program.cs
+++ sem_2/002/Program.cs
@@ -2,14 +2,6 @@
 // 14 -> нет
 // 46 -> нет
 // 161 -> да
-Console.WriteLine(" Введите число: ");
 int value = int.Parse(Console.ReadLine());
-if (value % 7 == 0 && value % 23 == 0) // именно двойное == , здесь остаток от деления 
+Console.WriteLine(value % 7 == 0 && value % 23 == 0); // именно двойное == , здесь остаток от деления 
 // на 7 и 23 должен РАВНЯТЬСЯ 0
-{
-    Console.WriteLine("Да");
-}
-else 
-{
-    Console.WriteLine("Нет");
-}
