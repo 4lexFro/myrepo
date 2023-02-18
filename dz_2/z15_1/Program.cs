c84a7016420ec36b13b48c81ee9ea4ec001454cb
Alexey Frolov <k43bfg@gmail.com>
2023-02-18 12:16:05 +0300

    Задача №15 Дни недели

---
+++ dz_2/z15_1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет      с методом
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+bool Vihod(int day)
+{
+    if (day > 5)
+    {
+        return true;
+    }
+
+    return false;
+}
+bool CheckDay(int number)
+{
+    if (number > 0 && number <= 7)
+    {
+        return true;
+    }
+    Console.WriteLine(" Это не день недели! ");
+    return false;
+}
+int day = Prompt(" Введите число: ");
+
+if (CheckDay(day))
+{
+    if (Vihod(day))
+    {
+        Console.WriteLine(" Выходной ");
+    }
+    else
+    {
+        Console.WriteLine(" Рабочий ");
+    }
+}
