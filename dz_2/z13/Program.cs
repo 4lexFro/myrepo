bc3c49dee593b3191db97f2f0733fbd1264a6928
Alexey Frolov <k43bfg@gmail.com>
2023-02-18 13:50:28 +0300

    Задача 13 Выводит третью цифру числа или говорит его нет

--- dz_2/z13/Program.cs
+++ dz_2/z13/Program.cs
@@ -1,8 +1,44 @@
-﻿// Задача 13: Напишите программу, которая выводит третью цифру 
-// заданного числа или сообщает, что третьей цифры нет.
-// 645 -> 5
-// 78 -> третьей цифры нет
-// 32679 -> 6
+﻿Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-Console.Write(" Введите число: ");
-int value = Convert.ToInt32(Console.ReadLine());
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+
+Решение:
+--------
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+ 
+int Find3Num(int number)
+{ 
+    wile (number > 999);
+    {
+       number /= 10;
+    }
+    return number % 10;
+}
+
+bool CheckNumber (int number)
+{
+    if (number < 100)
+    {
+       Console.WriteLine(" Третьей цифры нет ");
+       return false;
+    }
+    return true;
+}  
+
+int number = Prompt(" Введите число: ");
+if (CheckNumber(number))
+
+{
+    Console.WriteLine(Find3Num(number));
+}
