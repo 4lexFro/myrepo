bf14acac486e242b9c13614e16a847ca951d5278
Alexey Frolov <k43bfg@gmail.com>
2023-02-18 14:57:09 +0300

    Четные числа от 1 до N с методом

---
+++ sem_2/003_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Показать четные числа от 1 до N
+int N = 12;
+// int index = 2;
+//while (index <= N)
+//{
+//    Console.WriteLine(index);
+//    index = index + 2
+//}
+
+void EvenNumbers(int N)
+{
+    int index = 2;
+    while (index <= N)
+    {
+        System.Console.WriteLine(index);
+        index = index + 2;
+    }
+}
+
+EvenNumbers(N);
