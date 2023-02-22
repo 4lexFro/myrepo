99ddeea465e010025593335ed790a77bc222cb39
Alexey Frolov <k43bfg@gmail.com>
2023-02-22 16:14:46 +0300

    Задача 25: Напишите метод, который принимает на вход два числа (A и B)  и возводит число A в натуральную степень B

---
+++ dz4/z25/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 25: Напишите метод, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+// нельзя использовать Math.Pow
+// 3, 5 -> 243 // 3 в пятой степени
+// 2, 4 -> 16
+
+
+int nomerA = ReadInt("Введите число: ");
+int nomerB = ReadInt("Введите степень: ");
+Stepen(nomerA, nomerB);
+
+// Метод возведения в степень:
+static void Stepen(int x, int 
+y)
+{
+    int result = 1;
+    for (int i = 1; i <= y; i++)
+    {
+        result = result * x;
+    }
+    Console.WriteLine(" Результат : " + result);
+}
+
+// Метод ввода чисел:
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
--- dz_3/z23/Program.cs
+++ dz_3/z23/Program.cs
@@ -2,6 +2,13 @@
 //Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 //3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
 Console.WriteLine(" Введите число: ");
 int number = int.Parse(Console.ReadLine());
 int index = 1;
