7abe0a7aeb9154a9c0c71f93f423d1b4f3d9a2a1
Alexey Frolov <k43bfg@gmail.com>
2023-02-25 12:47:26 +0300

    Незавершенная 27 задача

---
+++ dz4/z27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите метод, который принимает на вход число
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine(" Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumElements(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+    }
+    return sum;
+}
+
+int summa = SumElements(number);
+Console.WriteLine("Сумма : " + summa);
