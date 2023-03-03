1d7bc2cb29079896dc6c65c14d3a55dedb442cbb
Alexey Frolov <k43bfg@gmail.com>
2023-03-03 13:54:10 +0300

    Задача 43 Найти координаты пересечения заданных пользователем прямых

--- dz_6/z43/Program.cs
+++ dz_6/z43/Program.cs
@@ -1,4 +1,10 @@
-﻿int GetNumber(string text)
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями
+// y = k1 * x + b1,  y = k2 * x + b2;  значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+int GetNumber(string text)
 {
     System.Console.WriteLine(text);
     return Convert.ToInt32(Console.ReadLine());
