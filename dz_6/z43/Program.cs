2f755a3385911c36c925aa49d0b03a95d3ea04a6
Alexey Frolov <k43bfg@gmail.com>
2023-03-03 13:48:51 +0300

    Задача 43 Найти координаты пересечения заданных пользователем прямых

---
+++ dz_6/z43/Program.cs
@@ -0,0 +1,33 @@
+﻿int GetNumber(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int xAxis(int b1, int b2, int k1, int k2)
+{
+    int x = (b1 - b2) / (k2 - k1);
+    
+    return x;
+}
+
+
+int yAxis( int k2, int b2, int x)
+{
+    int y = k2 * x + b2;
+    return y;
+}
+
+
+void PrintResult( string text, int x, int y)
+{
+    System.Console.WriteLine($"{text}:({x}, {y})");
+}
+
+int k1 = GetNumber("Введите k1");
+int k2 = GetNumber("Введите k2");
+int b1 = GetNumber("Введите b1");
+int b2 = GetNumber("Введите b2");
+int x = xAxis(b1, b2, k1, k2);
+int y = yAxis(k2, b2, x);
+PrintResult("Прямые пересекаются в точке:", x, y);
