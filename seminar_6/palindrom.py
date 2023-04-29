0d18d0320f7bff13badaec9e2594a40207ba29f1
alexey_frolov <k43bfg@gmail.com>
2023-04-29 19:40:40 +0300

    Проверка на палиндром рекурсией

--- seminar_6/palindrom.py
+++ seminar_6/palindrom.py
@@ -1,8 +1,11 @@
 list = input("Введите слово: ")
+    
 def palindrome(x):
     if len(x) <= 1: # если длина слова равна единице - нашли середину слова
         return True
     elif x[0] == x[-1]: # если первый элемент равен последнему
+        print(x)        
         return palindrome(x[1: -1]) # рекурсия с аргументами второго и предпоследнего элементов
+    
     return False
 print (palindrome(list))
